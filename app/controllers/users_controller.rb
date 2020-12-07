cf28ac3680288276741df4280a9d12d8f6c6a7a5
Zprogrammer-code <skatboyza124@hotmail.com>
2020-12-06 22:32:58 -0700

    first action of my restful convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
-class UsersController < ApplicationController
+
+module Api
+module v1
+    
+    class UsersController < ApplicationController
+
+    def index
+        users = Users.order('created_at DESC');
+        render json: {status:'SUCCESS', message: 'Loaded users', data:users, status: 'ok'}
+    end
 end
