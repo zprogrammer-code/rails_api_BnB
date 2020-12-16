0f636e1abbd8b032e6a14e77a6e373e29bdca362
Zprogrammer-code <skatboyza124@hotmail.com>
2020-12-15 17:43:13 -0700

    saving, adding methods to users controller, for a restful convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 
 module Api
-module v1
+module V1
     
     class UsersController < ApplicationController
 
     def index
         users = Users.order('created_at DESC');
-        render json: {status:'SUCCESS', message: 'Loaded users', data:users, status: 'ok'}
+        render json: {status:'SUCCESS', message: 'Loaded users', data:users}, status: :ok
+    end
+
+    def create
+        user = Users.create(
+            {
+                username: 'username',
+                password: 'password'
+            }
+        )
+        render json: {status:created,
+                        user: user}
     end
 end
