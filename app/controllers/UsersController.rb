c3968408215a655baa5137de123bf73eb16ab8e2
Zprogrammer-code <skatboyza124@hotmail.com>
2021-03-07 00:49:31 -0700

    just finished User controller, will now move on to workout model

--- app/controllers/UsersController.rb
+++ app/controllers/UsersController.rb
@@ -7,23 +7,44 @@
             render json: {@users}
         end
   
+        def show
+            @user = User.find(params[:id])
+            render json: {@user}
+        end
 
-    def create
-        @user = User.create(
-            {
-                username: 'username',
-                password: 'password'
-            }
-        )
-        render json: {status:created,
+    
+        def create
+            @user = User.create(user_params)
+            if @user.save
+                render json: {status:created,
                         user: @user}
-    end
+        end
+
+        def update
+            @user = user.find(params[:id])
+            if @user
+                @user.update(user_params)
+                render json: {message: 'user successfully updated', status: updated}
+            else
+                render json: {message: 'unable to update user '}
+        end
+
+        def destroy
+            @user = user.find(params[:id])
+            if @user
+                @user.destroy
+                render json: {message: 'user successfully destroyed', status: destroyed}
+            else
+                render json: {message: 'unable to delete user' }
+                
+            end
+        end
 
 
   private
 
   def user_params
     #Whitelisting for strng parameters
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :qualification, :college, :stream, :phone)
+    params.require(:user).permit(:username, :password)
   end
 end
