e88875a1ee8c78c936e7be161c6804f2999e310e
Zprogrammer-code <skatboyza124@hotmail.com>
2021-03-07 15:07:34 -0700

    added a restful convention in photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,56 @@
 class PhotosController < ApplicationController
+    def index
+        @photos = Photo.all
+        render json: {@photos}
+    end
+
+    def show
+        @photo = Photo.find(params[:id])
+        render json: {@photo}
+    end
+
+
+    def create
+        @photo = Photo.create(photo_params)
+        if @photo
+            render json: {status:created,
+                    photo: @photo}
+        else
+            render json: {message: 'unable to create photo'}
+            
+        end
+    end
+
+    def update
+        @photo = Photo.find(params[:id])
+        if @photo
+            @photo.update(photo_params)
+            render json: {message: 'photo successfully updated', status: updated}
+        else
+            render json: {message: 'unable to update photo '}
+    end
+
+    def destroy
+        @photo = Photo.find(params[:id])
+        if @photao
+            @photo.destroy
+            render json: {message: 'photo successfully destroyed', status: destroyed}
+        else
+            render json: {message: 'unable to delete photo' }
+            
+        end
+    end
+
+
+private
+
+def photo_params
+#Whitelisting for strng parameters
+params.require(:photo).permit(:username, :password)
+end
+
+def find_photo
+    @photo = Photo.find(params[:id])
+end
+
 end
