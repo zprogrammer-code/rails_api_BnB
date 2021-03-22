af134877ddbc8a205c3bb75829962926fde7a62e
Zprogrammer-code <skatboyza124@hotmail.com>
2021-03-22 09:32:07 -0600

    added resources for each model in my routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
   resources :users
+  resources :workouts
+  resources :photos
+  resources :training_season
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     end
   end
